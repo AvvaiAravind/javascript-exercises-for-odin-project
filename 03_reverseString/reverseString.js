41796e78e856ac228d105b3c4ec75f8ffe8b5a93
Avvai Aravind <avvaiaravind@gmail.com>
2024-09-20 14:45:36 +0530

    added split with empty quote and added reverse and added join with empty quote

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let string = str.split("");
+  string = string.reverse();
+  return (string = string.join(""));
 };
 
 // Do not edit below this line
