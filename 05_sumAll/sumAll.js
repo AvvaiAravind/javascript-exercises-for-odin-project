7fca7d4d7c2ddec4d6c7f1436abfac0c5d4f2698
Avvai Aravind <avvaiaravind@gmail.com>
2024-09-20 17:12:56 +0530

    added if and else clause to through error used for loop for adding all the integer

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  let result = 0;
+  let lesser = 0;
+  let larger = 0;
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2) ||
+    Array.isArray(num1) ||
+    Array.isArray(num2) ||
+    typeof num1 === "string" ||
+    typeof num2 === "string"
+  ) {
+    return "ERROR";
+  } else if (num1 > num2) {
+    lesser = num2;
+    larger = num1;
+  } else if (num1 < num2) {
+    lesser = num1;
+    larger = num2;
+  } else {
+    return "ERROR";
+  }
+  for (i = lesser; i <= larger; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
