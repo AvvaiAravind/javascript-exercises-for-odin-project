59279def7691cce9b566820d6c4d6edec04afe88
Avvai Aravind <avvaiaravind@gmail.com>
2024-09-20 16:09:59 +0530

    added filter and used remove function to remove the values from the array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function (arrays, ...value) {
+  //const index = arrays.indexOf(value);
+  //const arr1 = arrays.slice(0, index);
+  //const arr2 = arrays.slice(index + 1);
+  //return arr1.concat(arr2);
+  const arr = arrays;
+  const result = arr.filter(remove);
+
+  function remove(arr) {
+    let newArr = [];
+    if (!value.includes(arr)) {
+      return (newArr += arr);
+    }
+  }
+
+  return result;
 };
 
 // Do not edit below this line
