7a4eecfcfaf018a1900e957a2296cbfa30fe7b93
Avvai Aravind <avvaiaravind@gmail.com>
2024-09-25 14:57:52 +0530

    created palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+  let str1 = [...words]
+    .reverse()
+    .join("")
+    .replace(/[^A-Za-z0-9]/g, "")
+    .toLowerCase();
+  let str2 = words.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+  return str1 === str2;
 };
 
 // Do not edit below this line
