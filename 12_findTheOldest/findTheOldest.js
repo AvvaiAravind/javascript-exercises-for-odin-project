642fab40b44d45c15e354b2244bf4649e51de817
Avvai Aravind <avvaiaravind@gmail.com>
2024-09-25 18:42:58 +0530

    added the finding oldest person logic

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arrays) {
+  let currentYear = new Date().getFullYear();
+  const oldest = arrays.reduce((old, cur) => {
+    let older =
+      (old.yearOfDeath ? old.yearOfDeath : currentYear) - old.yearOfBirth;
+    let current =
+      (cur.yearOfDeath ? cur.yearOfDeath : currentYear) - cur.yearOfBirth;
+    return current > older ? cur : old;
+  });
+  return oldest;
 };
 
 // Do not edit below this line
