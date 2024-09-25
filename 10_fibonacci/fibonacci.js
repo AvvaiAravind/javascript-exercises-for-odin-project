bd85f32202f5be1fd028a34fb69e9f3b798451b7
Avvai Aravind <avvaiaravind@gmail.com>
2024-09-25 15:56:11 +0530

    added finbonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (arg) {
+  let a = 0;
+  let b = 1;
+  let total = 0;
+  let num = parseInt(arg);
+  if (num < 0) return "OOPs";
+  if (num === 0 || num === 1) return num;
+  for (let i = 2; i < num; i++) {
+    total = a + b;
+    a = b;
+    b = total;
+  }
+  return total;
 };
 
 // Do not edit below this line
